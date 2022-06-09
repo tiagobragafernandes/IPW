a98a4d56e826485e7d344f2d1d4b0c3436389bfa
Tiago Fernandes <Tiago Fernandes>
2022-06-08 22:37:08 -0300

    subindo o index.html

---
+++ assets/script/loginAuth.js
@@ -0,0 +1,27 @@
+function verificaLogin(){
+    const login = "juliocesar@email.com";
+    const senha = "123456789";
+    const loginDigitado = document.getElementById('auth-email').value;
+    const senhaDigitada = document.getElementById('auth-pass').value;
+
+
+    if(login == loginDigitado && senha == senhaDigitada){
+        window.location.href = "../../home_client.html"
+
+    }else{
+        alert("Login ou Senha incorretos, tente novamente.")
+    }
+}
+
+function verificaCadastro(){
+    const loginDigitado = document.getElementById('auth-email').value;
+    const senhaDigitada = document.getElementById('auth-pass').value;
+
+    if(loginDigitado != null && senhaDigitada != null){
+        if(loginDigitado != "" && senhaDigitada != ""){
+            window.location.href = "../../home_client.html"
+        }else{
+            alert("Por favor, preencha todos os campos corretamente.")
+    }
+    }
+}
